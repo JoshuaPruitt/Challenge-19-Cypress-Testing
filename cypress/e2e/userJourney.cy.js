660948e1a6a5e8277d16fc1cee40d01b5786d2f1
Joshua <joshuapruitt6484@gmail.com>
2024-10-21 16:11:16 -0700

    added code for testing questions

--- cypress/e2e/userJourney.cy.js
+++ cypress/e2e/userJourney.cy.js
@@ -10,22 +10,55 @@ describe("User Journey", () => {
   it("A user can click the start button and the quiz will start.", () => {
     //click the start button
     cy.get("div button").click();
-    //check that the questions display on the page
-    cy.get(".alert .alert-secondary").should(($el) => {
-      const questions = $el.map((i, el) => {
-        return Cypress.$(el).attr("alert-secondary");
-      });
-      questions.each((i, question) => {
-        expect(question).to("be.visible");
-      });
-    });
+    //check that there are 4 questions on the page now
+    cy.get("div div")
+      .eq(1)
+      .children()
+      .eq(1)
+      .children()
+      .should("have.length", 4);
   });
 
   it("A user can answer questions and new questions will be displayed on the page", () => {
     //click the start button
     cy.get("div button").click();
 
-    //click the quiz question button
-    cy.get(".mt-3").children().eq(0).click();
+    //get the questions
+    cy.get("div div")
+      .eq(1)
+      .children()
+      .eq(1)
+      .children()
+      .eq(1)
+      .invoke("text")
+      .then((text) => {
+        //click the quiz question button
+        cy.get("div button").eq(0).click();
+
+        //get the new question 1 and check that the old question 1 does not have the same text
+        cy.get("div div")
+          .eq(1)
+          .children()
+          .eq(1)
+          .children()
+          .eq(1)
+          .invoke("text")
+          .should("not.equal", text);
+      });
+  });
+
+  it("When all questions are answered then the quiz is over.", () => {
+    //click the quiz start button
+    cy.get("div button").click();
+
+    for (let x = 0; x < 10; x++) {
+      //click the quiz question button
+      cy.get("div button").eq(0).click();
+    }
+
+    //once everything check that we are now on the score page
+    cy.get("alert-success").should(($el) => {
+      expect($el).to("be.visible");
+    });
   });
 });
